82118710affc33051a362fe3e8d8eb0d38be7b68
LiHowe <howe.personal@gmail.com>
2022-04-20 17:55:31 +0800

    feat: function: invokeFnWithCatch

---
+++ src/function/invokeFnWithCatch.ts
@@ -0,0 +1,18 @@
+/**
+ * 使用 try...catch 包裹调用方法
+ * @param fn any function
+ * @param onError error handler
+ * @returns 
+ */
+export function invokeFnWithCatch<T>(
+  fn: (...args: any) => T,
+  onError?: (error: any) => void
+  ): T | void {
+    try {
+      return fn()
+    } catch (e) {
+      if (onError) {
+        onError(e)
+      }
+    }
+  }
